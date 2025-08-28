46755b737c962ec61d38a9786d484c6463026657
Bism@786@786 <2014tabontech@gmail.com>
2025-08-28 16:48:59 +0500

    done

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
+// stop passing in HOST, so we can remove this workaround after the next major release
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
